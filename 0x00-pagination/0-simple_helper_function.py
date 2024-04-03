43c526e406bab64416ceca7d152fa257fae7fea2
Stephanie <Stephaniechloe21@gmail.com>
2024-04-03 17:34:03 +0100

    task 1

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,2 +1,9 @@
+#!/usr/bin/env python3
+""" Index Range"""
+
+
 def index_range(page, page_size):
+    """
+    A function that returns a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters
+    """
     return ((page - 1) * page_size, page * page_size)
