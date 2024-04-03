0916e4a2330592e694341e53189e6961472ab156
Stephanie <Stephaniechloe21@gmail.com>
2024-04-03 18:28:23 +0100

    hmm

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,6 +3,15 @@ import csv
 import math
 from typing import List
 
+
+def index_range(page, page_size):
+    """
+    A function that returns a tuple of size two containing
+    a start index and an end index
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
 class Server:
     """Server class to paginate a database of popular baby names."""
 
@@ -21,22 +30,11 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int):
-        """
-        return a tuple of size two containing a
-        start index and an end index corresponding to the
-        range of indexes to return in a list
-        for those particular pagination parameters.
-        """
-        end_index = page * page_size
-        return (end_index - page_size, end_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''
             Paginates dataset
         '''
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
-
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page_size > 0 and page > 0
         (startIndex, endIndex) = index_range(page, page_size)
         return self.dataset()[startIndex:endIndex]
