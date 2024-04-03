4960a57e69372c50e678d12d1c474e20c567a5d7
Stephanie <Stephaniechloe21@gmail.com>
2024-04-03 17:37:45 +0100

    fix

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,7 @@
 
 def index_range(page, page_size):
     """
-    A function that returns a tuple of size two containing a start index and an end index corresponding to the range of indexes to return in a list for those particular pagination parameters
+    A function that returns a tuple of size two containing
+    a start index and an end index
     """
     return ((page - 1) * page_size, page * page_size)
