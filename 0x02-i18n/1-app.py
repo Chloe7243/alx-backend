f21b82486f18f3a079fe86cd60c13fa8d8c749b4
Stephanie <Stephaniechloe21@gmail.com>
2024-04-09 20:34:25 +0100

    task 2

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Web Flask """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    ''' App Configuration class
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__, template_folder="templates")
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """ Hello world """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
